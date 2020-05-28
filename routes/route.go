eb402ea9019f4b2838b7830848aa4a92f0c0de24
dzlzh <dzlzh@null.net>
2020-05-28 11:23:52 +0800

    cors

--- routes/route.go
+++ routes/route.go
@@ -2,8 +2,10 @@ package routes
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/dzlzh/gin-example/utils"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,20 @@ var Router *gin.Engine
 
 func init() {
 	Router = gin.Default()
+
+	// 跨域
+	mwCORS := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Type"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 2400 * time.Hour,
+	})
+	Router.Use(mwCORS)
 }
 
 func SetupRouter() *gin.Engine {
